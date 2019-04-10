c798aef01c890fd9880b3996e4c92617579aeca2
ummyers <mu.andrea@ciencias.unam.mx>
2019-04-10 00:44:22 -0500

    Coordenadas NoFake

---
+++ Base/temas.sql
@@ -0,0 +1,14 @@
+INSERT INTO informador (correo,nombre,contrasenia) VALUES ('jhovan.gallardo@ciencias.unam.mx','Jhovan','password');
+INSERT INTO tema (nombre, color, correo_informador) VALUES ('CCHs', '#808000',  'jhovan.gallardo@ciencias.unam.mx');
+INSERT INTO marcador (longitud, latitud, descripcion, datos_utiles, nombre_tema) VALUES (-99.142100, 19.484092,  'CCH Vallejo', 'Plantes del colegio de ciencias y humanidades ubicado en Vallejo.','CCHs');
+INSERT INTO marcador (longitud, latitud, descripcion, datos_utiles, nombre_tema) VALUES (-99.204575, 19.500106,  'CCH Azcapotzalco', 'Plantes del colegio de ciencias y humanidades ubicado en Azcapotzalco.','CCHs');
+INSERT INTO marcador (longitud, latitud, descripcion, datos_utiles, nombre_tema) VALUES (-99.058625, 19.384205,  'CCH Oriente', 'Plantes del colegio de ciencias y humanidades ubicado en Oriente.','CCHs');
+INSERT INTO marcador (longitud, latitud, descripcion, datos_utiles, nombre_tema) VALUES (-99.199244, 19.312320,  'CCH Sur', 'Plantes del colegio de ciencias y humanidades ubicado en Sur.','CCHs');
+
+INSERT INTO informador (correo,nombre,contrasenia) VALUES ('mu.andrea@ciencias.unam.mx','Andrea','12345');
+INSERT INTO tema (nombre, color, correo_informador) VALUES ('ENP', '#808fff',  'mu.andrea@ciencias.unam.mx');
+INSERT INTO marcador (longitud, latitud, descripcion, datos_utiles, nombre_tema) VALUES (-99.121773, 19.271162, 'ENP 1', 'Plantel Gabino Barreda ubicado en Xochimilco', 'ENP');
+INSERT INTO marcador (longitud, latitud, descripcion, datos_utiles, nombre_tema) VALUES (-99.099383, 19.383909, 'ENP 2', 'Plantel Erasmo Castellanos Quinto ubicado en Churubusco', 'ENP');
+INSERT INTO marcador (longitud, latitud, descripcion, datos_utiles, nombre_tema) VALUES (19.482775, -99.095528, 'ENP 3', 'Plantel Justo Sierra ubicado en Constitución', 'ENP');
+INSERT INTO marcador (longitud, latitud, descripcion, datos_utiles, nombre_tema) VALUES (19.403626, -99.195481, 'ENP 4', 'Plantel Vidal Castañeda y Najera ubicado en Observatorio', 'ENP');
+INSERT INTO marcador (longitud, latitud, descripcion, datos_utiles, nombre_tema) VALUES (-99.134713, 19.307540, 'ENP 5', 'Plantel José Vasconcelos ubicado en Coapa', 'ENP');
